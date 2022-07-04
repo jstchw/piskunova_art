aed90e9781702c700c632ecacfabdb8591089711
jstchw <bobr703@gmail.com>
2022-07-04 15:58:03 +0300

    Initial dump of the webapp

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def main():
+    insta_profile = 'https://www.instagram.com/piskunovaart/'
+    github_profile = 'https://github.com/jstchw'
+    year = date.today().year
+    return render_template('main.html', insta_profile=insta_profile, year=year, github_profile=github_profile)
+
+
+# @app.route('/theatre')
+# def theatre():
+#     return render_template('templates_obsolete/theatre.html')
+#
+#
+# @app.route('/media')
+# def media():
+#     return render_template('templates_obsolete/media.html')
+
+
+if __name__ == '__main__':
+    app.run()
