07962f686fa84c49b50f86cdd3ba0f0f24cbf7a8
jstchw <bobr703@gmail.com>
2022-07-04 16:51:43 +0300

    Design works (icons, section etc.)

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app = Flask(__name__, template_folder='templates')
 def main():
     insta_profile = 'https://www.instagram.com/piskunovaart/'
     github_profile = 'https://github.com/jstchw'
+    in_profile = 'https://www.linkedin.com/in/elizaveta-piskunova-3a745b244/'
     year = date.today().year
-    return render_template('main.html', insta_profile=insta_profile, year=year, github_profile=github_profile)
+    return render_template('main.html', insta_profile=insta_profile, year=year, github_profile=github_profile,
+                           in_profile=in_profile)
 
 
 # @app.route('/theatre')
